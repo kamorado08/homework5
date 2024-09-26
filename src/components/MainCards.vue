1439dcd7fba0d2e4b2f95a33f392d9ace55f0c57
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-09-25 19:22:10 -0500

    fixed rounded corners on image and card

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -3,7 +3,9 @@ import MainCardSingle from './MainCardSingle.vue';
 </script>
 
 <template>
-
-    <MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
+    <!-- chosing grid, grid column 5, space between each picture 5, padding 10-->
+<div class="grid grid-cols-5 gap-10 p-10">
+   <MainCardSingle v-for="card in 20" :key="card"></MainCardSingle> 
+</div>
 
 </template>
