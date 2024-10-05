86c2ce38429653885fd8538b2b19429e30835e1f
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-10-05 06:38:13 -0500

    CardDetails Done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,9 +1,10 @@
 <script setup>
 //importing faker to create random names, pictures, and bios 
 import { faker } from '@faker-js/faker'
-const fullname = faker.person.fullName()
+
 const firstname = faker.person.firstName()
 const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({firstName: firstname, lastName: lastname})
 const job_title = faker.person.jobTitle()
 const bio = faker.lorem.lines(2)
 const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
@@ -13,6 +14,7 @@ const username = faker.internet.userName({firstName: firstname, lastName: lastna
     
 <template>
     <!-- {{ fullname }}<br /> -->
+    <RouterLink :to="{name: 'CardDetails', params: {id: username} }">
     <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
         <img :src="faker.image.urlLoremFlickr({category: 'cat'})" class="rounded-t-xl" />
         <div class="p-4">
@@ -20,7 +22,7 @@ const username = faker.internet.userName({firstName: firstname, lastName: lastna
             <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
             <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
         </div>
-
     </div>
+    </RouterLink>
 
 </template>
