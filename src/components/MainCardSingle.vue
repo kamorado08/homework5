be7d4b67224383409d58da5451b419065e7f6bd0
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-10-06 12:39:56 -0500

    Added styles to pagination

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -16,7 +16,7 @@ const username = faker.internet.userName({firstName: firstname, lastName: lastna
     <!-- {{ fullname }}<br /> -->
     <RouterLink :to="{name: 'CardDetails', params: {id: username} }">
     <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
-        <img :src="faker.image.urlLoremFlickr({category: 'cat'})" class="rounded-t-xl" />
+        <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'cat'})" class="rounded-t-xl" />
         <div class="p-4">
             <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
             <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
